b50d9fc00716f839688ee5d3c05270b393c9610a
Jingmeng Zhang <Jingmeng@Sakura.(none)>
2016-07-14 21:33:02 -0700

    add new part for view2 and factory service.

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -6,14 +6,41 @@ angular.module('myApp.detail', ['ngRoute', 'myApp.list'])
     controller: 'View2Ctrl'
   });
 }])
+.directive('myEnter', function($timeout) {
+	return function (scope, elem, attrs) {
+		elem.bind("keydown keypress", function(e) {
+			//prevent empty input
+			if (scope.newMessage == undefined || scope.newMessage == null || scope.newMessage == '') {
+				return;
+			}
 
+			if (e.which === 13) {
+				scope.$apply(function() {
+					scope.$eval(attrs.myEnter);
+				});
+				scope.$watchCollection(myEnter, function(newValue) {
+					if (newValue) {
+						$(elem).scrollTop(elem[0].scrollHeight);
+					}
+
+				});
+				e.preventDefault();
+			}
+		});
+	};
+})
 .controller('View2Ctrl', ['$scope', '$routeParams', 'messages', '$rootScope', function($scope, $routeParams, messages, $rootScope) {
 	$rootScope.location = "#!/view2";
 	$rootScope.loginId = 90001;
 	$scope.info = messages.getDetail($routeParams.id);
 	
 	$scope.send = function(m) {
-		
+		var connectorId = $routeParams.id;
+		var d = (new Date()).toJSON();
+		var s = messages.createNewSentence($rootScope.loginId, d, m);
+		messages.sendMsgIn(connectorId, s);
 
+		//clear the input box
+		$scope.newMessage = null;
 	};
 }]);
