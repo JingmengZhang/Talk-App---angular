6e890f3cdfdca4ad621007a8caf1633dfa9f1f96
Jingmeng Zhang <Jingmeng@Sakura.(none)>
2016-07-15 17:48:14 -0700

    update filter for view2 and factory service.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -27,8 +27,8 @@ angular.module('myApp.list', ['ngRoute'])
 		return result;
 	}
 })
-.filter('lastMsg', function($filter) {
-	return function(input) {
+.filter('lastMsg', function() {
+	return function(input) {	
 		if (input == null || input.length == 0){return "";}
 		var result, i, temp, saveInfo;
 		saveInfo = input[0];
@@ -39,15 +39,15 @@ angular.module('myApp.list', ['ngRoute'])
 			}
 		}
 		result = saveInfo.text;
-		if (result.length > 48) {
-			result = result.substr(0, 46) + '...';
+		if (result.length != null && result.length > 38) {
+			result = result.substr(0, 34) + '...';
 		}
 		return result;
 	}
 })
 .factory('messages', ['$http', function($http) {
 	//fake factory API for loading data
-	var data = [{"id":900001,"userId":10001,"name":"Ryan Lian","title":"Product Manager","company":"Huawei","location":"Beijing, China","datetime":"2016-07-13T18:25:43.511Z","contents":[{"id":990001,"userId":10001,"datetime":"2016-07-11T12:25:43.511Z","text":"How's going?","readed":true},{"id":990002,"userId":90001,"datetime":"2016-07-11T12:26:43.511Z","text":"Pretty good. How about you?","readed":true},{"id":990003,"userId":10001,"datetime":"2016-07-11T12:29:43.511Z","text":"wonderful. When do we go to lunch, today?","readed":true},{"id":990004,"userId":90001,"datetime":"2016-07-11T12:30:43.511Z","text":"How about right now??","readed":true}],"url":"img/ticon.jpg"},{"id":900002,"userId":10002,"name":"Bai Xie","title":"Former Teaching Assistant","company":"SDSU","location":"Los Angeles, California","datetime":"2015-04-23T18:30:43.511Z","contents":[{"id":990001,"userId":10002,"datetime":"2015-04-23T18:25:43.511Z","text":"How's going?","readed":true},{"id":990002,"userId":90001,"datetime":"2015-04-23T18:26:43.511Z","text":"Pretty good. How about you?","readed":true},{"id":990003,"userId":10002,"datetime":"2015-04-23T18:29:43.511Z","text":"wonderful. When do we go to lunch, today?","readed":true},{"id":990004,"userId":90001,"datetime":"2015-04-23T18:30:43.511Z","text":"Let's have it right now. I'm really hungray. What do you want to eat?","readed":true}],"url":"img/sicon.jpg"},{"id":900003,"userId":90001,"name":"Weiwei, Susan","title":"","company":"","location":"","datetime":"2012-04-23T20:30:43.511Z","contents":[{"id":990001,"userId":10003,"datetime":"2012-04-23T20:25:43.511Z","text":"Who wants to go shopping?","readed":true},{"id":990002,"userId":90001,"datetime":"2012-04-23T20:26:43.511Z","text":"Which shopping mall r you going?","readed":true},{"id":990003,"userId":10004,"datetime":"2012-04-23T20:29:43.511Z","text":"wonderful. Add me on","readed":true},{"id":990004,"userId":90001,"datetime":"2012-04-23T20:30:43.511Z","text":"Santana Row? How about on Saturday?","readed":true},{"id":990005,"userId":10004,"datetime":"2012-04-23T20:30:43.511Z","text":"I'm ok with it!","readed":true},{"id":990006,"userId":10004,"datetime":"2012-04-23T20:30:43.511Z","text":"When would be a good time for us to meet","readed":true}],"url":"img/sicon.png"}];
+	var data = [{"id":900001,"userId":10001,"name":"Ryan Lian","title":"Product Manager","company":"Huawei","location":"Beijing, China","datetime":"2016-07-13T18:25:43.511Z","group":false,"contents":[{"userId":10001,"datetime":"2016-07-11T12:25:43.511Z","text":"How's going?","readed":true},{"userId":90001,"datetime":"2016-07-11T12:26:43.511Z","text":"Pretty good. How about you?","readed":true},{"userId":10001,"datetime":"2016-07-11T12:29:43.511Z","text":"wonderful. When do we go to lunch, today?","readed":true},{"userId":90001,"datetime":"2016-07-11T12:30:43.511Z","text":"How about right now??","readed":true}],"url":"img/ticon.jpg"},{"id":900002,"userId":10002,"name":"Bai Xie","title":"Former Teaching Assistant","company":"SDSU","location":"Los Angeles, California","datetime":"2015-04-23T18:30:43.511Z","group":false,"contents":[{"userId":10002,"datetime":"2015-04-23T18:25:43.511Z","text":"How's going?","readed":true},{"userId":90001,"datetime":"2015-04-23T18:26:43.511Z","text":"Pretty good. How about you?","readed":true},{"userId":10002,"datetime":"2015-04-23T18:29:43.511Z","text":"wonderful. When do we go to lunch, today?","readed":true},{"userId":90001,"datetime":"2015-04-23T18:30:43.511Z","text":"Let's have it right now. I'm really hungray. What do you want to eat?","readed":true}],"url":"img/sicon.jpg"},{"id":900003,"userId":90001,"name":"Weiwei, Susan","title":"","company":"","location":"","datetime":"2012-04-23T20:30:43.511Z","group":true,"contents":[{"userId":10003,"datetime":"2012-04-23T20:25:43.511Z","text":"Who wants to go shopping?","readed":true},{"userId":90001,"datetime":"2012-04-23T20:26:43.511Z","text":"Which shopping mall r you going?","readed":true},{"userId":10004,"datetime":"2012-04-23T20:29:43.511Z","text":"wonderful. Add me on","readed":true},{"userId":90001,"datetime":"2012-04-23T20:30:43.511Z","text":"Santana Row? How about on Saturday?","readed":true},{"userId":10004,"datetime":"2012-04-23T20:30:43.511Z","text":"I'm ok with it!","readed":true},{"userId":10004,"datetime":"2012-04-23T20:30:43.511Z","text":"When would be a good time for us to meet","readed":true}],"url":"img/sicon.png"}];
 
 	//construction for each sentence
 	function Sentence(id, dttm, txt) {
@@ -77,15 +77,20 @@ angular.module('myApp.list', ['ngRoute'])
 			return new Sentence(id, dttm, txt);
 		},
 
-		sendMsgIn: function(id, s) {
-			var dlg, i;
+		sendMsgIn: function(id, s, dt) {
+			if (id == null || s == null) 
+				return false;
+
+			var dlg, i, target;
 			dlg = {};
 			for (i=0; i<data.length; i++) {
-				if (data[i].userId == id) {
-					dlg = data[i].contents;
+				target = data[i];
+				if (target.userId == id) {
+					target.datetime = dt;
+					dlg = target.contents;
+					dlg.push(s);
 				}
 			}
-			dlg.push(s);
 			return true;
 		}
 
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -6,6 +6,39 @@ angular.module('myApp.detail', ['ngRoute', 'myApp.list'])
     controller: 'View2Ctrl'
   });
 }])
+.filter('widthFixed', function() {
+	function formatMsg(input) {	
+		if (input == null || input.length == 0){return '';}
+		var r, i;
+		r = '';
+		while (input.length != null && input.length > 0) {
+			if (input.length > 31) {
+				r = r + input.slice(0, 30)+ '\n';
+				input = input.substring(30);
+			} else {
+				r += input;
+				input = '';
+			}
+		}
+		return r;
+	}
+
+	return function(input) {	
+		if (input == null || input.length == 0){return "";}
+		var result, i, temp, arr;
+		arr = input.split(' ');
+		for (i=0; i<arr.length; i++) {
+			temp = arr[i];
+			if (temp.length > 31) {
+				result = formatMsg(temp);
+				arr[i] = result;
+			}
+		}
+		result = arr.join(' ');
+
+		return result;
+	}
+})
 .directive('myEnter', function($timeout) {
 	return function (scope, elem, attrs) {
 		elem.bind("keydown keypress", function(e) {
@@ -13,34 +46,38 @@ angular.module('myApp.detail', ['ngRoute', 'myApp.list'])
 			if (scope.newMessage == undefined || scope.newMessage == null || scope.newMessage == '') {
 				return;
 			}
-
 			if (e.which === 13) {
 				scope.$apply(function() {
 					scope.$eval(attrs.myEnter);
-				});
-				scope.$watchCollection(myEnter, function(newValue) {
-					if (newValue) {
-						$(elem).scrollTop(elem[0].scrollHeight);
-					}
-
 				});
 				e.preventDefault();
 			}
 		});
 	};
 })
-.controller('View2Ctrl', ['$scope', '$routeParams', 'messages', '$rootScope', function($scope, $routeParams, messages, $rootScope) {
+.controller('View2Ctrl', ['$scope', '$routeParams', 'messages', '$rootScope', '$location', '$anchorScroll', function($scope, $routeParams, messages, $rootScope, $location, $anchorScroll) {
 	$rootScope.location = "#!/view2";
 	$rootScope.loginId = 90001;
 	$scope.info = messages.getDetail($routeParams.id);
 	
-	$scope.send = function(m) {
-		var connectorId = $routeParams.id;
-		var d = (new Date()).toJSON();
-		var s = messages.createNewSentence($rootScope.loginId, d, m);
-		messages.sendMsgIn(connectorId, s);
-
-		//clear the input box
-		$scope.newMessage = null;
+	$scope.send = function(msg) {
+		var connectorId, d, s, r;
+		connectorId = $routeParams.id;
+		d = (new Date()).toJSON();
+		s = messages.createNewSentence($rootScope.loginId, d, msg);
+		r = messages.sendMsgIn(connectorId, s, d);
+		if (r) {
+			var newHash = 'anchor' + s.datetime;
+			if ($location.hash() !== newHash) {
+				$location.hash(newHash);
+			} else {
+				$anchorScroll();
+			}
+			
+			//clear the input box
+			$scope.newMessage = null;
+			//$scope.newMessage.focus();
+		}
 	};
+
 }]);
